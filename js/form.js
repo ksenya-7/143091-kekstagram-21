c804a482ab01c57ae03662a176aa37395ac42ce0
Ksu <ksenya-7@yandex.ru>
2020-11-01 19:09:38 +0300

    исправления 2

--- js/form.js
+++ js/form.js
@@ -1,7 +1,7 @@
 "use strict";
 
+const MIN_HASHTAG_LENGTH = 2;
 const MAX_HASHTAG_LENGTH = 20;
-const MIN_HASHTAG_LENGTH = 20;
 const MAX_DESCRIPTION_LENGTH = 140;
 const FILE_TYPES = [`gif`, `jpg`, `jpeg`, `png`];
 
@@ -61,12 +61,10 @@ uploadFile.addEventListener(`change`, () => {
 });
 
 // валидация
-const re = /^\s*#\w*$/;
-
-uploadForm.addEventListener(`submit`, (evt) => {
+textHashtagsInput.addEventListener(`input`, (evt) => {
+  const re = /^\s*#\w*$/;
   const hashTag = textHashtagsInput.value;
   const hashTags = hashTag.split(` `);
-  const description = textDescriptionInput.value;
 
   const hashTagSet = new Set();
   for (let element of hashTags) {
@@ -74,36 +72,46 @@ uploadForm.addEventListener(`submit`, (evt) => {
   }
 
   const isRepeatOfTag = hashTagSet.size < hashTags.length;
-  const isInvalidOfTag = textHashtagsInput.value === `` ? false : hashTags.some((element) => !re.test(element));
-  const isMinLengthOfTag = hashTags.some((element) => element.length > MAX_HASHTAG_LENGTH);
-  const isMaxLengthOfTag = hashTags.some((element) => element.length < MIN_HASHTAG_LENGTH);
-  const isLengthOfDescription = description.length > MAX_DESCRIPTION_LENGTH ? true : false;
-
-  const listenChangesOfForm = () => {
-    textHashtagsInput.setCustomValidity(``);
-    textDescriptionInput.setCustomValidity(``);
-    textHashtagsInput.style.outline = `none`;
-    textDescriptionInput.style.outline = `none`;
-    textHashtagsInput.reportValidity();
-    textDescriptionInput.reportValidity();
-  };
-
-  uploadForm.addEventListener(`keydown`, listenChangesOfForm);
+  const isInvalidOfTag = hashTag === `` ? false : hashTags.some((element) => !re.test(element));
+  const isMaxLengthOfTag = hashTags.some((element) => element.length > MAX_HASHTAG_LENGTH);
+  const isMinLengthOfTag = hashTag === `` ? false : hashTags.some((element) => element.length < MIN_HASHTAG_LENGTH);
 
   evt.preventDefault();
   if (isInvalidOfTag || hashTags.length > 5) {
+    textHashtagsInput.style.outline = `2px solid red`;
     textHashtagsInput.setCustomValidity(`Строка после решётки должна состоять из букв и чисел и не может содержать пробелы, спецсимволы (#, @, $ и т. п.), символы пунктуации (тире, дефис, запятая и т. п.), эмодзи и т. д. Нельзя указать больше пяти хэш-тегов.`);
-    textHashtagsInput.style.outline = `2px solid red`;
   } else if (isRepeatOfTag) {
+    textHashtagsInput.style.outline = `2px solid red`;
     textHashtagsInput.setCustomValidity(`Хэш-теги нечувствительны к регистру: #ХэшТег и #хэштег считаются одним и тем же тегом. Один и тот же хэш-тег не может быть использован дважды.`);
+  } else if (isMaxLengthOfTag || isMinLengthOfTag) {
     textHashtagsInput.style.outline = `2px solid red`;
-  } else if (isMinLengthOfTag || isMaxLengthOfTag) {
     textHashtagsInput.setCustomValidity(`Минимальная длина одного хэш-тега – 2 символа, максимальная длина – 20 символов, включая решётку.`);
-    textHashtagsInput.style.outline = `2px solid red`;
-  } else if (isLengthOfDescription) {
+  } else {
+    textHashtagsInput.setCustomValidity(``);
+    textHashtagsInput.style.outline = `none`;
+  }
+
+  textHashtagsInput.reportValidity();
+});
+
+textDescriptionInput.addEventListener(`input`, (evt) => {
+  const description = textDescriptionInput.value;
+  const isLengthOfDescription = description.length > MAX_DESCRIPTION_LENGTH ? true : false;
+  evt.preventDefault();
+  if (isLengthOfDescription) {
     textDescriptionInput.setCustomValidity(`Максимальная длина комментария 140 символов. Удалите лишние ${description.length - MAX_DESCRIPTION_LENGTH} симв.`);
     textDescriptionInput.style.outline = `2px solid red`;
-  } else if (!matches) {
+  } else {
+    textDescriptionInput.setCustomValidity(``);
+    textDescriptionInput.style.outline = `none`;
+  }
+
+  textDescriptionInput.reportValidity();
+});
+
+uploadForm.addEventListener(`submit`, (evt) => {
+  evt.preventDefault();
+  if (!matches) {
     uploadOverlay.classList.add(`hidden`);
     window.successError.openErrorMessage();
   } else {
